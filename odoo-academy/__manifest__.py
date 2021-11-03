9c7d01521a29c384d738a099ba9d516d4a58d6e2
adda-odoo <adda@odoo.com>
2021-11-03 18:29:36 +0000

    [ADD] Odoo-Academy

---
+++ odoo-academy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Manage training""",
+    
+    'description': 'Testing Module Creation',
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [],
+    
+    'demo': [],
+}
